7f1deed4a2008eeeae3b485f012164aac0fe7e6c
salva <salvationordeath@gmail.com>
2023-01-03 17:12:38 +0700

    Первый коммит

---
+++ src/test/java/com/example/webdevelopment_part3/WebDevelopmentPart3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webdevelopment_part3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDevelopmentPart3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
